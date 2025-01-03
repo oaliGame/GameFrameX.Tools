b2bce28df68742d245f0430f67f94074e53f6442
Blank <wangfj11@foxmail.com>
2025-01-03 12:02:38 +0800

    [修改]1. 修改协议导出函数抽象

--- ProtoExport/Program.cs
+++ ProtoExport/Program.cs
@@ -4,8 +4,6 @@ namespace GameFrameX.ProtoExport
 {
     internal static class Program
     {
-        private static IProtoGenerateHelper ProtoGenerateHelper;
-
         private static int Main(string[] args)
         {
             var launcherOptions = Parser.Default.ParseArguments<LauncherOptions>(args).Value;
@@ -21,46 +19,7 @@ namespace GameFrameX.ProtoExport
                 return 1;
             }
 
-            if (!Directory.Exists(launcherOptions.OutputPath))
-            {
-                Directory.CreateDirectory(launcherOptions.OutputPath);
-            }
-
-
-            var types = typeof(IProtoGenerateHelper).Assembly.GetTypes();
-            foreach (var type in types)
-            {
-                var attrs = type.GetCustomAttributes(typeof(ModeAttribute), true);
-                if (attrs?.Length > 0 && (attrs[0] is ModeAttribute modeAttribute) && modeAttribute.Mode == modeType)
-                {
-                    ProtoGenerateHelper = (IProtoGenerateHelper)Activator.CreateInstance(type);
-                    break;
-                }
-            }
-
-            var files = Directory.GetFiles(launcherOptions.InputPath, "*.proto", SearchOption.AllDirectories);
-
-            var messageInfoLists = new List<MessageInfoList>(files.Length);
-            foreach (var file in files)
-            {
-                var operationCodeInfo = MessageHelper.Parse(File.ReadAllText(file), Path.GetFileNameWithoutExtension(file), launcherOptions.OutputPath, launcherOptions.IsGenerateErrorCode);
-                messageInfoLists.Add(operationCodeInfo);
-                switch (modeType)
-                {
-                    case ModeType.Server:
-                    case ModeType.Unity:
-                        ProtoGenerateHelper?.Run(operationCodeInfo, launcherOptions.OutputPath, launcherOptions.NamespaceName);
-                        break;
-                    case ModeType.TypeScript:
-                        ProtoGenerateHelper?.Run(operationCodeInfo, launcherOptions.OutputPath, Path.GetFileNameWithoutExtension(file));
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            ProtoGenerateHelper?.Post(messageInfoLists, launcherOptions.OutputPath);
-
+            ProtoBufMessageHandler.Start(launcherOptions, modeType);
 
             Console.WriteLine("导出成功，按任意键退出");
             Console.ReadKey();
---
+++ ProtoExport/ProtoBufMessageHandler.cs
@@ -0,0 +1,48 @@
+﻿namespace GameFrameX.ProtoExport;
+
+public static class ProtoBufMessageHandler
+{
+    private static IProtoGenerateHelper ProtoGenerateHelper;
+
+    public static void Start(LauncherOptions launcherOptions, ModeType modeType)
+    {
+        if (!Directory.Exists(launcherOptions.OutputPath))
+        {
+            Directory.CreateDirectory(launcherOptions.OutputPath);
+        }
+
+        var types = typeof(IProtoGenerateHelper).Assembly.GetTypes();
+        foreach (var type in types)
+        {
+            var attrs = type.GetCustomAttributes(typeof(ModeAttribute), true);
+            if (attrs?.Length > 0 && (attrs[0] is ModeAttribute modeAttribute) && modeAttribute.Mode == modeType)
+            {
+                ProtoGenerateHelper = (IProtoGenerateHelper)Activator.CreateInstance(type);
+                break;
+            }
+        }
+
+        var files = Directory.GetFiles(launcherOptions.InputPath, "*.proto", SearchOption.AllDirectories);
+
+        var messageInfoLists = new List<MessageInfoList>(files.Length);
+        foreach (var file in files)
+        {
+            var operationCodeInfo = MessageHelper.Parse(File.ReadAllText(file), Path.GetFileNameWithoutExtension(file), launcherOptions.OutputPath, launcherOptions.IsGenerateErrorCode);
+            messageInfoLists.Add(operationCodeInfo);
+            switch (modeType)
+            {
+                case ModeType.Server:
+                case ModeType.Unity:
+                    ProtoGenerateHelper?.Run(operationCodeInfo, launcherOptions.OutputPath, launcherOptions.NamespaceName);
+                    break;
+                case ModeType.TypeScript:
+                    ProtoGenerateHelper?.Run(operationCodeInfo, launcherOptions.OutputPath, Path.GetFileNameWithoutExtension(file));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        ProtoGenerateHelper?.Post(messageInfoLists, launcherOptions.OutputPath);
+    }
+}
