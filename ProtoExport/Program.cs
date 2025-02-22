adfddf15de8c5386b44ebfb3200f14182f27d2a0
Blank <wangfj11@foxmail.com>
2025-02-22 11:41:43 +0800

    [增加]1. 增加导出协议文件的错误处理

--- ProtoExport/Program.cs
+++ ProtoExport/Program.cs
@@ -6,23 +6,32 @@ namespace GameFrameX.ProtoExport
     {
         private static int Main(string[] args)
         {
-            var launcherOptions = Parser.Default.ParseArguments<LauncherOptions>(args).Value;
-            if (launcherOptions == null)
+            try
             {
-                Console.WriteLine("参数错误，解析失败");
-                return 1;
+                var launcherOptions = Parser.Default.ParseArguments<LauncherOptions>(args).Value;
+                if (launcherOptions == null)
+                {
+                    Console.WriteLine("参数错误，解析失败");
+                    throw new Exception("参数错误，解析失败");
+                }
+
+                if (!Enum.TryParse<ModeType>(launcherOptions.Mode, true, out var modeType))
+                {
+                    Console.WriteLine("不支持的运行模式");
+                    throw new Exception("不支持的运行模式");
+                }
+
+                ProtoBufMessageHandler.Start(launcherOptions, modeType);
+                Console.WriteLine("导出成功,请查看日志");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("导出失败,请检查错误信息");
+                Console.WriteLine(e);
+                Console.ReadKey();
+                throw;
             }
 
-            if (!Enum.TryParse<ModeType>(launcherOptions.Mode, true, out var modeType))
-            {
-                Console.WriteLine("不支持的运行模式");
-                return 1;
-            }
-
-            ProtoBufMessageHandler.Start(launcherOptions, modeType);
-
-            Console.WriteLine("导出成功，按任意键退出");
-            Console.ReadKey();
             return 0;
         }
     }
