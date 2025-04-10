3e4341ad3677d8657c6179cb9b96f79e88498e50
oaliGame <oali.cloud.svr1@gmail.com>
2025-04-10 23:21:14 +0800

    --self-contained false

--- publish_proto_export.bat
+++ publish_proto_export.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet publish ProtoExport/ProtoExport.csproj -c Release -r win-x64 --self-contained true -o ./bin
+dotnet publish ProtoExport/ProtoExport.csproj -c Release -r win-x64 --self-contained false -o ./bin
